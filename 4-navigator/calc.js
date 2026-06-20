61623076206f246fc11d3f5a28af235b90f4043d
Mona Lisa~ <pantelnn@gmail.com>
2026-06-20 05:55:39 +0300

    Добавлено решение

---
+++ 4-navigator/calc.js
@@ -0,0 +1,6 @@
+const addressLat = 3;
+const addressLong = 4;
+const positionLat = 1;
+const positionLong = 2;
+
+const result = Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2)
