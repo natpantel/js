c8f4f997b483e2bf61af52ef1080ea6c34278704
Mona Lisa~ <pantelnn@gmail.com>
2026-06-20 15:19:31 +0300

    Добавлено решение

---
+++ 5-language/language.js
@@ -0,0 +1,16 @@
+const language = 'jkljl'
+
+switch (language) {
+    case "en":
+        console.log("Hello!");
+        break;
+    case "ru":
+        console.log("Привет!");
+        break;  
+    case "de":
+        console.log("Hallo!");
+        break;  
+    default:
+        console.log("???");
+}   
+
