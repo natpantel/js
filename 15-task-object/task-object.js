12821f4c2b8eea29cbe052e7a0b1c9c03107fc4a
Mona Lisa~ <pantelnn@gmail.com>
2026-06-26 11:43:22 +0300

    Добавлено решение

---
+++ 15-task-object/task-object.js
@@ -0,0 +1,35 @@
+const toDoList = {
+    tasks: [
+        {
+            "title": "Помыть посуду",
+            id: 1,
+            priority: 1
+        }
+    ], 
+    getLastId: function () {
+        let lastId = -1;
+        for (let task of this.tasks) {
+            if (task.id > lastId) {
+                lastId = task.id;
+            }
+        }
+        lastId++;
+        return lastId
+    },
+    addTask: function (title, priority) {
+        this.tasks.push({title, priority, id: this.getLastId()});
+    },
+    deleteTask: function (id) {
+        this.tasks = this.tasks.filter(el => el.id != id)
+    },
+    updateTask: function (id, newTitle) {
+        for (let task of this.tasks) {
+            if (task.id == id) {
+                task.title = newTitle
+            }
+        };
+    },
+    sortTasks: function() {
+        this.tasks.sort((a, b) => {a.priority > b.priority})
+    }
+}
